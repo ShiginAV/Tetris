8246cd909b6e2146a38755816b1bbdd7debe34e5
ShiginAV <a.sh.10031987@gmail.com>
2017-07-21 15:42:12 +0300

    1

--- Block.cpp
+++ Block.cpp
@@ -5,7 +5,7 @@ Block::Block(int _x, int _y) : x(_x), y(_y)
 	texture.loadFromFile("images/tiles.png");
 }
 
-//отобразить блок
+
 void Block::paint(RenderWindow &win, int colorNum)
 {
 	sprite.setTexture(texture);
